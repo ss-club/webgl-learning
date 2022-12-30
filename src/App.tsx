d3f1ab70dab8365bec6a3cdf498c94bc410bb141
Shuai Sun <shuai.sun@smartmore.com>
2022-12-30 17:16:44 +0800

    feat: init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import { initShaders } from "./lib/cuon-utils";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const mountRef = useRef<HTMLCanvasElement>(null);
+
+  var VSHADER_SOURCE =
+    "void main() {\n" +
+    "  gl_Position = vec4(0.0, 0.0, 0.0, 1.0);\n" + // Set the vertex coordinates of the point
+    "  gl_PointSize = 10.0;\n" + // Set the point size
+    "}\n";
+
+  // Fragment shader program
+  var FSHADER_SOURCE =
+    "void main() {\n" +
+    "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n" + // Set the point color
+    "}\n";
+  useEffect(() => {
+    if (mountRef.current) {
+      const gl = mountRef.current.getContext("webgl");
+      if (!gl) {
+        console.log("Failed to get the rendering context for WebGL");
+        return;
+      }
+
+      // Initialize shaders
+      if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log("Failed to intialize shaders.");
+        return;
+      }
+
+      // Specify the color for clearing <canvas>
+      gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+      // Clear <canvas>
+      gl.clear(gl.COLOR_BUFFER_BIT);
+
+      // Draw a point
+      gl.drawArrays(gl.POINTS, 0, 1);
+    }
+  }, []);
+
+  return <canvas width={400} height={400} ref={mountRef}></canvas>;
 }
 
 export default App;
