90eb97928560ed2ad9f58acb3a6ed982030b8426
Shuai Sun <shuai.sun@smartmore.com>
2023-01-04 15:59:02 +0800

    feat: draw colored points

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import "./App.css";
 function App() {
   const mountRef = useRef<HTMLCanvasElement>(null);
 
-  const pointRef = useRef<number[]>([]);
+  const pointRef = useRef<number[][]>([]);
+  const colorRef = useRef<number[][]>([]);
 
   var VSHADER_SOURCE =
     "attribute vec4 a_Position;\n" +
@@ -16,8 +17,10 @@ function App() {
 
   // Fragment shader program
   var FSHADER_SOURCE =
+    "precision mediump float;\n" +
+    "uniform vec4 u_FragColor;\n " +
     "void main() {\n" +
-    "  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n" + // Set the point color
+    "  gl_FragColor = u_FragColor;\n" + // Set the point color
     "}\n";
   useEffect(() => {
     if (mountRef.current) {
@@ -38,31 +41,55 @@ function App() {
         "a_Position"
       );
 
+      const u_FragColor = gl.getUniformLocation(
+        (gl as any).program,
+        "u_FragColor"
+      );
+
       if (a_Position < 0) {
         console.log("failure to get a_Position");
         return;
       }
 
+      if (!u_FragColor) {
+        console.log("failure to get u_FragColor");
+        return;
+      }
       const handleClick = (
         e: MouseEvent,
         gl: WebGLRenderingContext,
         canvas: HTMLCanvasElement,
-        a_Position: any
+        a_Position: any,
+        u_FragColor: WebGLUniformLocation
       ) => {
         let x = e.offsetX;
         let y = e.offsetY;
         x = ((x - canvas.width / 2) / canvas.width) * 2;
         y = ((canvas.height / 2 - y) / canvas.height) * 2;
-        pointRef.current.push(x);
-        pointRef.current.push(y);
+        pointRef.current.push([x, y]);
+
+        if (x >= 0.0 && y >= 0.0) {
+          colorRef.current.push([1.0, 0.0, 0.0, 1.0]);
+        } else if (x < 0.0 && y < 0.0) {
+          colorRef.current.push([0.0, 1.0, 0.0, 1.0]);
+        } else {
+          colorRef.current.push([0.0, 0.0, 1.0, 1.0]);
+        }
 
         gl.clear(gl.COLOR_BUFFER_BIT);
-        for (let i = 0; i < pointRef.current.length; i += 2) {
+        for (let i = 0; i < pointRef.current.length; i++) {
           gl.vertexAttrib3f(
             a_Position,
-            pointRef.current[i],
-            pointRef.current[i + 1],
+            pointRef.current[i][0],
+            pointRef.current[i][1],
             0.0
+          );
+          gl.uniform4f(
+            u_FragColor,
+            colorRef.current[i][0],
+            colorRef.current[i][1],
+            colorRef.current[i][2],
+            colorRef.current[i][3]
           );
 
           gl.drawArrays(gl.POINTS, 0, 1);
@@ -70,10 +97,9 @@ function App() {
       };
 
       mountRef.current.addEventListener("click", (e) => {
-        handleClick(e, gl, mountRef.current!, a_Position);
+        handleClick(e, gl, mountRef.current!, a_Position, u_FragColor);
       });
 
-      gl.vertexAttrib3f(a_Position, 0.0, 0.0, 0.0);
       // Specify the color for clearing <canvas>
       gl.clearColor(0.0, 0.0, 0.0, 1.0);
 
@@ -81,7 +107,6 @@ function App() {
       gl.clear(gl.COLOR_BUFFER_BIT);
 
       // Draw a point
-      gl.drawArrays(gl.POINTS, 0, 1);
     }
   }, []);
 
